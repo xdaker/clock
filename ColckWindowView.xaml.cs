a8f42d8186b041b20646f706537aaa709c2b5645
YG <359923820@qq.com>
2016-10-03 18:44:44 +0800

    添加Colck库

--- ColckWindowView.xaml.cs
+++ ColckWindowView.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Clock
+namespace Colck
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ColckWindowView : Window
     {
-        public MainWindow()
+        public ColckWindowView()
         {
             InitializeComponent();
         }
