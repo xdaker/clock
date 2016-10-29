41729b9fb15ccd5e0b422fd229e0d01dd07693ac
xdaker <359923820@qq.com>
2016-10-29 11:52:52 +0800

    添加wifi功能

--- Clock/Clock/Misc/CreateCmd.cs
+++ Clock/Clock/Misc/CreateCmd.cs
@@ -9,30 +9,37 @@ namespace Clock.Misc
     {
         public static string create(string str)
         {
-            //process用于调用外部程序
-            System.Diagnostics.Process p = new System.Diagnostics.Process();
-            //调用cmd.exe
-            p.StartInfo.FileName = "cmd.exe";
-            //是否指定操作系统外壳进程启动程序
-            p.StartInfo.UseShellExecute = false;
-            //可能接受来自调用程序的输入信息
-            //重定向标准输入
-            p.StartInfo.RedirectStandardInput = true;
-            //重定向标准输出
-            p.StartInfo.RedirectStandardOutput = true;
-            //重定向错误输出
-            p.StartInfo.RedirectStandardError = true;
-            //不显示程序窗口
-            p.StartInfo.CreateNoWindow = true;
-            //启动程序
-            p.Start();
-            //输入命令
-            p.StandardInput.WriteLine(str);
-            //一定要关闭。
-            System.Threading.Thread.Sleep(500);
-            p.StandardInput.WriteLine("exit");
-            p.Close();
-            return p.StandardOutput.ReadToEnd();
+            try
+            {
+                //process用于调用外部程序
+                System.Diagnostics.Process p = new System.Diagnostics.Process();
+                //调用cmd.exe
+                p.StartInfo.FileName = "cmd.exe";
+                //是否指定操作系统外壳进程启动程序
+                p.StartInfo.UseShellExecute = false;
+                //可能接受来自调用程序的输入信息
+                //重定向标准输入
+                p.StartInfo.RedirectStandardInput = true;
+                //重定向标准输出
+                p.StartInfo.RedirectStandardOutput = true;
+                //重定向错误输出
+                p.StartInfo.RedirectStandardError = true;
+                //不显示程序窗口
+                p.StartInfo.CreateNoWindow = true;
+                //启动程序
+                p.Start();
+                //输入命令
+                p.StandardInput.WriteLine(str);
+                //一定要关闭。
+                //System.Threading.Thread.Sleep(500);
+                p.StandardInput.WriteLine("exit");
+
+                return p.StandardOutput.ReadToEnd();
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
         }
     }
 }
